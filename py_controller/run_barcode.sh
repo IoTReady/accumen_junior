402b864c210da22c07c140723d268a9081c2e8f7
Tej Pochiraju <tej@iotready.co>
2022-02-21 15:24:11 +0530

    Added separate scripts for running camera & barcode

---
+++ py_controller/run_barcode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo /usr/local/bin/accumen.sh
+source ./venv/bin/activate
+sudo ./venv/bin/python barcode/scan.py 
---
+++ py_controller/run_camera.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo /usr/local/bin/accumen.sh
+source ./venv/bin/activate
+./venv/bin/python app.py --device 3
