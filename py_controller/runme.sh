66eef137f042c3cff6f64eb93f08c10e48f3349a
Tej Pochiraju <tej@iotready.co>
2022-03-04 15:24:30 +0530

    Update runme with path

--- py_controller/runme.sh
+++ py_controller/runme.sh
@@ -2,5 +2,5 @@
 
 sudo /usr/local/bin/accumen.sh
 source ./venv/bin/activate
-./venv/bin/python app.py --device 3 &
+./venv/bin/python app.py --device 0 --path /home/ccmsadmin/workspace/original_image &
 sudo ./venv/bin/python barcode/scan.py 
