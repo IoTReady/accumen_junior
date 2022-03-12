0481564b960addaa646b29de7d7e4bbab9bc4db1
Tej Pochiraju <tej@iotready.co>
2022-03-12 15:33:31 +0530

    Working flow + move to upython idf3

--- remote_io/app.py
+++ remote_io/app.py
@@ -7,7 +7,7 @@ from utime import sleep_ms, ticks_ms
 from esp32_i2c_lcd import I2cLcd
 from uping import ping
 
-VERSION = "0.0.1"
+VERSION = "0.1.0"
 
 lan = None
 
@@ -16,7 +16,7 @@ is_triggering = False
 
 config = {
     "ip": "192.168.10.2",
-    "gateway": "192.168.10.1",
+    "gateway": "10.10.20.1",
     "netmask": "255.255.255.0",
     "base_url": "http://192.168.10.1:8000",
     "limitswitch": 36,
@@ -35,17 +35,20 @@ config = {
     "status_row_1": 1,
     "status_row_2": 2,
     "status_row_3": 3,
-    "healthcheck_interval": 30
+    "healthcheck_interval": 30,
 }
 
 mac_address = "".join("{:02x}".format(d) for d in unique_id()).upper()
 
-
+gpio17 = Pin(17, Pin.OUT)
+gpio17.value(False)
+sleep_ms(500)
+gpio17 = Pin(17, Pin.IN)
 
 status_led_1 = Pin(int(config.get("status_led_1")), Pin.OUT)
 status_led_2 = Pin(int(config.get("status_led_2")), Pin.OUT)
 neopixel_pin = Pin(int(config.get("neopixel")), Pin.OUT)
-limitswitch = Pin(int(config.get("button")), Pin.IN)
+limitswitch = Pin(int(config.get("limitswitch")), Pin.IN)
 scl = Pin(int(config.get("i2c_scl")), Pin.OUT)
 sda = Pin(int(config.get("i2c_sda")), Pin.OUT)
 i2c = SoftI2C(scl=scl, sda=sda)
@@ -61,7 +64,7 @@ lcd = I2cLcd(
 lcd.custom_char(0, bytearray([0x0, 0x1, 0x3, 0x16, 0x1C, 0x8, 0x0, 0x0]))
 
 # Neopixel array
-neopixel_array = neopixel.Neopixel(neopixel_pin, config.get('num_pixels'))
+neopixel_array = neopixel.NeoPixel(neopixel_pin, config.get("num_pixels"))
 
 last_interrupt_time = 0
 previous_trigger_ticks = 0
@@ -75,10 +78,11 @@ def limitswitch_isr(pin: Pin):
         trigger()
     last_interrupt_time = interrupt_time
 
+
 limitswitch.irq(limitswitch_isr, trigger=Pin.IRQ_FALLING)
 
 
-def set_status_led(led:Pin, state: bool):
+def set_status_led(led: Pin, state: bool):
     # Inverted logic
     led.value(not state)
 
@@ -89,15 +93,16 @@ def sync_tray_led():
 
 
 def switch_off_neopixels():
-    neopixel_array.fill((0,0,0))
+    neopixel_array.fill((0, 0, 0))
 
 
 def switch_on_neopixels():
     switch_off_neopixels()
-    positions = [0,1,2,14,15,16,28,29,30,42,43,44]
+    positions = [0, 1, 2, 14, 15, 16, 28, 29, 30, 42, 43, 44]
     for i in positions:
         neopixel_array[i] = (255, 255, 220)
     neopixel_array.write()
+
 
 # Display functions
 def clear_lcd_line(line_num: int):
@@ -180,7 +185,7 @@ def display_connecting():
     row = config.get("status_row_3")
     clear_lcd_line(row)
     lcd.move_to(0, row)
-    lcd.putstr("Connecting to PC")
+    lcd.putstr("Connecting to PC...")
     gc.collect()
 
 
@@ -205,7 +210,8 @@ def display_disconnected():
     lcd.move_to(0, row)
     lcd.putstr("Check PC Connection")
     gc.collect()
-    #print("D: Heap Memory: ", gc.mem_free())
+    # print("D: Heap Memory: ", gc.mem_free())
+
 
 # End display functions
 
@@ -218,11 +224,23 @@ def backup_app():
     destination = "/app_previous.py"
     shutil.copyfile(source, destination)
 
+
 def connect_lan():
-    lan = network.LAN(mdc=Pin(23), mdio=Pin(18), power=Pin(12), phy_type = network.PHY_LAN8720, phy_addr=0, clock_mode=network.ETH_CLOCK_GPIO17_OUT)
+    global lan
+    display_connecting()
+    lan = network.LAN(
+        mdc=Pin(23),
+        mdio=Pin(18),
+        power=Pin(12),
+        phy_type=network.PHY_LAN8720,
+        phy_addr=0,
+        clock_mode=network.ETH_CLOCK_GPIO17_OUT,
+    )
     # optionally, use static config
     # lan.ifconfig(config.get('ip'), config.get('gateway'), config.get('netmask'))
     lan.active(1)
+    sleep_ms(10000)
+
 
 def download_ota():
     print("W: Starting OTA")
@@ -234,7 +252,7 @@ def download_ota():
     gc.collect()
     display_ota_updating()
     base_url = config.get("base_url")
-    url = f"{base_url}/app.py"
+    url = "{}/app.py".format(base_url)
     res = requests.get(url)
     if res.status_code != 200 or not res.content:
         display_ota_error()
@@ -259,23 +277,33 @@ def set_clock():
     except Exception as e:
         print("E:", str(e))
 
+
 def trigger():
     global is_triggering
     global previous_trigger_ticks
     limit_switch_state = limitswitch.value()
-    if not is_triggering and not limit_switch_state and is_connected and ticks_ms() - previous_trigger_ticks >= trigger_interval:
+    if (
+        not is_triggering
+        and not limit_switch_state
+        and is_connected
+        and ticks_ms() - previous_trigger_ticks >= trigger_interval
+    ):
         is_triggering = True
         set_status_led(status_led_2, True)
         display_triggered()
-        url = f"{config.get('base_url')}/trigger"
-        res = requests.post(url)
-        if res.status_code == 200:
+        url = "{}/trigger".format(config.get("base_url"))
+        print(url)
+        try:
+            res = requests.post(url)
+            print("res",res)
             sleep_ms(1000)
+            assert res.status_code == 200, "Capture Error"
             display_captured()
-            sleep_ms(2000)
-            display_waiting()
-        else:
+        except Exception as e:
+            print(str(e))
             display_error()
+        sleep_ms(2000)
+        display_waiting()
         set_status_led(status_led_2, False)
         is_triggering = False
         previous_trigger_ticks = ticks_ms()
@@ -283,21 +311,36 @@ def trigger():
 
 def healthcheck():
     global is_connected
-    if check_ping():
-        display_connected()
-        is_connected = True
-    else:
+    try:
+        if check_ping():
+            if is_connected == False:
+                display_connected()
+                if not is_triggering:
+                    display_waiting()
+            # Post a request over HTTP
+            is_connected = True
+        else:
+            display_disconnected()
+            is_connected = False
+            # sleep_ms(2000)
+            # connect_lan()
+    except Exception as e:
         display_disconnected()
-        is_connected = False 
-        sleep_ms(2000)
-        connect_lan()
-    # Post a request over HTTP
+        is_connected = False
+        print("E: healthcheck:", str(e))
     gc.collect()
 
+
 timer1 = Timer(1)
-timer1.init(period=config.get('healthcheck_interval')*1000, mode=Timer.PERIODIC, callback=lambda t:healthcheck())
+timer1.init(
+    period=config.get("healthcheck_interval") * 1000,
+    mode=Timer.PERIODIC,
+    callback=lambda t: healthcheck(),
+)
+
 
 def start():
+    print("Starting app", __name__)
     try:
         count = 0
         lcd.clear()
@@ -314,13 +357,16 @@ def start():
         print("I: Ready!")
         print("I: App Version: ", VERSION)
         while True:
+            count += 1
             sync_tray_led()
-            if count > 10:
+            if count > 50:
+                print("GPIO17:", gpio17.value())
                 gc.collect()
                 print("D: Heap Memory: ", gc.mem_free())
-            count += 1
+                count = 0
             sleep_ms(int(config.get("main_loop_delay_ms")))
     except Exception as e:
+        print("E:", str(e))
         set_status_led(status_led_1, False)
         set_status_led(status_led_2, False)
         switch_off_neopixels()
--- remote_io/flash.sh
+++ remote_io/flash.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Uncomment lines below for clean flash
-esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
-esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 esp32-20220117-v1.18.bin
-sleep 5
-
-ampy -p /dev/ttyUSB0 put lcd_api.py
-echo "Uploaded lcd_api.py"
-ampy -p /dev/ttyUSB0 put esp32_i2c_lcd.py
-echo "Uploaded esp32_i2c_lcd.py"
-ampy -p /dev/ttyUSB0 put uping.py
-echo "Uploaded uping.py"
-ampy -p /dev/ttyUSB0 put shutil.py
-echo "Uploaded shutil.py"
+#esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
+##esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 esp32-20220117-v1.18.bin
+#esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 esp32-idf3-20210202-v1.14.bin
+#sleep 5
+##
+#ampy -p /dev/ttyUSB0 put lcd_api.py
+#echo "Uploaded lcd_api.py"
+#ampy -p /dev/ttyUSB0 put esp32_i2c_lcd.py
+#echo "Uploaded esp32_i2c_lcd.py"
+#ampy -p /dev/ttyUSB0 put uping.py
+#echo "Uploaded uping.py"
+#ampy -p /dev/ttyUSB0 put shutil.py
+#echo "Uploaded shutil.py"
 
 # Upload main.py
 ampy -p /dev/ttyUSB0 put app.py app_factory.py
