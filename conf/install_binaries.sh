922b711c5a8ca54dc90199d7446049944225f14c
Tej Pochiraju <tej@iotready.co>
2022-03-15 14:26:50 +0530

    Kill running services before installing updates

--- conf/install_binaries.sh
+++ conf/install_binaries.sh
@@ -9,11 +9,15 @@ sudo cp interfaces /etc/network/
 sudo apt install -y wget python3-pip
 sudo pip3 install adafruit-ampy esptool
 
+sudo systemctl stop barcode
+sudo pkill barcode
 sudo wget https://iotready-public.s3.ap-south-1.amazonaws.com/aira/accumen_junior/barcode.bin -O /usr/local/bin/barcode.bin
 sudo chmod +x /usr/local/bin/barcode.bin
 sudo cp barcode.service /etc/systemd/system/
 sudo systemctl enable barcode.service
 
+sudo systemctl stop camera
+sudo pkill camera
 sudo wget https://iotready-public.s3.ap-south-1.amazonaws.com/aira/accumen_junior/camera.bin -O /usr/local/bin/camera.bin
 sudo chmod +x /usr/local/bin/camera.bin
 sudo cp camera.sh /usr/local/bin/
