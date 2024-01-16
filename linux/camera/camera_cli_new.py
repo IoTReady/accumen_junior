9683ae489924b46e5df9aa811abc0de295dee7b9
manasseh-abraham <manasshe2@gmail.com>
2024-01-16 20:06:03 +0530

    cleanup v0.1

--- linux/camera/camera_cli_new.py
+++ linux/camera/camera_cli_new.py
@@ -4,16 +4,14 @@ from asyncio import sleep
 from numpy.core.fromnumeric import sort
 from pypylon import pylon
 from time import sleep
-
-
-from io import BytesIO
-from PIL import Image
-import numpy as np
+from datetime import datetime
 
 gain_redux = 0
+EXPOSURE = 10000
+GAIN = 1
 
 
-def init_camera():
+def initialise_camera():
     try:
         # Initialize the pylon runtime system (optional)
         # pylon.pylon_initialize()
@@ -25,10 +23,10 @@ def init_camera():
         camera.Open()
         
         # Set exposure time in microseconds
-        camera.ExposureTime.SetValue(10000)  # 10,000 microseconds (10 ms)
+        camera.ExposureTime.SetValue(EXPOSURE)  # 10,000 microseconds (10 ms)
         
         # Set gain value in dB
-        camera.Gain.SetValue(1)  # Set gain to 6 dB
+        camera.Gain.SetValue(GAIN)  # Set gain to 6 dB
         
         return camera, img_res
     except Exception as e:
@@ -45,55 +43,74 @@ def capture_frame(camera):
         return img
     except Exception as e:
         print(f"Error capturing frame: {e}")
-        return None
+        return e
 
 def save_image(camera,img, img_res):
-    global gain_redux      # remove
     try:
-        if sys.argv[2]:
-            num_img_to_save = int(sys.argv[2])
-        else:
-            num_img_to_save = 1
-        for i in range(num_img_to_save):
-            if img is not None and img.GrabSucceeded():
-                # Access the image data
-                #image_data = img.GetArray()
-                
-                # Save the image as a file (e.g., PNG)
-                #with open('captured_basler_image.png', 'wb') as f:
-                #    f.write(image_data.tobytes())
-                
-                #deafult pylon way
-                #image_data.Save(pylon.ImageFileFormat_Png, "captured_basler_image.png")
+        now = int(datetime.now().timestamp())
+        tmppath = f"/tmp/{now}.jpg"
+        if img is not None and img.GrabSucceeded():
+            # Access the image data
+            #image_data = img.GetArray()
+            
+            # Save the image as a file (e.g., PNG)
+            #with open('captured_basler_image.png', 'wb') as f:
+            #    f.write(image_data.tobytes())
+            
+            #deafult pylon way
+            #image_data.Save(pylon.ImageFileFormat_Png, "captured_basler_image.png")
 
-                img_res.AttachGrabResultBuffer(img)
-                print("Attached Grab results ...")
-                if sys.argv[1]:
-                    filename = f"90_img_{str(sys.argv[1])}_{i}.png" 
-                else:
-                    filename = f"90_img_{i}.png"
-                img_res.Save(pylon.ImageFileFormat_Png, filename)
+            img_res.AttachGrabResultBuffer(img)
+            print("Attached Grab results ...")
+            img_res.Save(pylon.ImageFileFormat_Png, tmppath)
 
-                #image_data = img.GetArray()
-                #grey_image = Image.fromarray(np.uint8(image_data),mode="L")
-                #pillow_image = grey_image.convert('RGB')
-                # Save the image as a file (e.g., PNG)
-                #pillow_image.save(filename)
+            #image_data = img.GetArray()
+            #grey_image = Image.fromarray(np.uint8(image_data),mode="L")
+            #pillow_image = grey_image.convert('RGB')
+            # Save the image as a file (e.g., PNG)
+            #pillow_image.save(filename)
 
 
-                print(f"Image Saved {filename}")
-                # Release the image
-                img.Release()
-                print("Trying to release the image")
-            sleep(1)
-            if camera.IsGrabbing():
-                camera.StopGrabbing()
-            #gain_redux += 0.15
-            #camera.Gain.SetValue(1.0-gain_redux)  # Set gain to 6 dB
-            #print(f"Camera Gain : {1.0-gain_redux}")
-            img = capture_frame(camera)
+            print(f"Image Saved {tmppath}")
+            # Release the image
+            img.Release()
+            print("Trying to release the image")
+        sleep(1)
+        if camera.IsGrabbing():
+            camera.StopGrabbing()
+        #gain_redux += 0.15
+        #camera.Gain.SetValue(1.0-gain_redux)  # Set gain to 6 dB
+        #print(f"Camera Gain : {1.0-gain_redux}")
+        img = capture_frame(camera)
+        return tmppath 
     except Exception as e:
         print(f"Error saving image: {e}")
+        return e
+
+def capture_optimised(camera, img_res):
+    try:
+        img = capture_frame(camera)
+        ERROR_MSG = None
+        PATH = None
+        try:
+            PATH = save_image(camera, img, img_res)
+        except Exception as e:
+            ERROR_MSG = e
+        ret = {
+                'brightness': None,
+                'hue': None,
+                'contrast': None,
+                'path': PATH,
+                'attempts': None,
+                'exposure': EXPOSURE,
+                'gain': GAIN,
+                'img': img,
+                'error': ERROR_MSG 
+            }
+        return ret
+    except Exception as e:
+        print(str(e))
+        return {"error": str(e)}
 
 def camera_close(camera):
     try:
@@ -111,9 +128,10 @@ if __name__ == "__main__":
     #cProfile.run("img = capture_frame(camera)", sort='cumulative')
     #cProfile.run("save_image(camera, img, img_res)", sort='cumulative')
     #cProfile.run("camera_close(camera)", sort='cumulative')
-    camera, img_res = init_camera()
+    camera, img_res = initialise_camera()
+    
     if camera:
-        img = capture_frame(camera)
-        cProfile.run("save_image(camera, img, img_res)",sort='cumulative')
+        ret = capture_optimised(camera,img_res)
+        #cProfile.run("save_image(camera, img, img_res)",sort='cumulative')
         camera_close(camera)
 
--- linux/camera/camera_gui_qt.py
+++ linux/camera/camera_gui_qt.py
@@ -1,9 +1,9 @@
 import sys
-#import cv2
+import time
 import numpy as np
 from pypylon import pylon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtGui import QImage, QPixmap, QPainter
 from PyQt5.QtCore import Qt, QTimer
 
 class CameraApp(QMainWindow):
@@ -40,6 +40,7 @@ class CameraApp(QMainWindow):
             img_res = pylon.PylonImage()
             camera.Open()
             camera.ExposureTime.SetValue(10000)  # 10,000 microseconds (10 ms)
+            camera.PixelFormat.SetValue("RGB8")
             camera.Gain.SetValue(1)  # Set gain to 6 dB
             return camera, img_res
         except Exception as e:
@@ -50,14 +51,14 @@ class CameraApp(QMainWindow):
         img = self.capture_frame_from_camera()
         if img:
             self.save_image(img)
-            print("Image saved")
+            print("sv File ")
 
     def capture_frame_from_camera(self):
         try:
-            print("capturing image")
+            if self.camera.IsGrabbing():
+                self.camera.StopGrabbing()
             self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
             img = self.camera.RetrieveResult(10000, pylon.TimeoutHandling_ThrowException)
-            print("got image")
             return img
         except Exception as e:
             print(f"Error capturing frame: {e}")
@@ -68,8 +69,9 @@ class CameraApp(QMainWindow):
             img_res = self.img_res 
             img_res.AttachGrabResultBuffer(img)
             print("saved image")
-            filename = "captured_image.png"
-            img_res.Save(pylon.ImageFileFormat_Png, filename)
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            filename = f"{timestr}.png"
+            img_res.Save(pylon.ImageFileFormat_Tiff, filename)
             print(f"Image saved as {filename}")
             img.Release()
         except Exception as e:
@@ -78,13 +80,17 @@ class CameraApp(QMainWindow):
     def update_preview(self):
         img = self.capture_frame_from_camera()
         if img:
-            image_data = img.GetArray()
-            #height, width, channel = image_data.shape
-            height, width = image_data.shape
-            bytes_per_line = 3 * width
-            q_image = QImage(image_data.data, width, height, bytes_per_line, QImage.Format_RGB888)
-            pixmap = QPixmap.fromImage(q_image)
-            self.preview_label.setPixmap(pixmap)
+            frame = np.ascontiguousarray(img.GetArray())
+            qframe = QImage(
+                frame.data,
+                frame.shape[1],
+                frame.shape[0],
+                frame.shape[1]*3,
+                QImage.Format_RGB888
+            )
+            pixmap = QPixmap.fromImage(qframe)
+            resized_pixmap = pixmap.scaled(1280, 720, Qt.KeepAspectRatio)
+            self.preview_label.setPixmap(resized_pixmap)
 
     def closeEvent(self, event):
         if self.camera.IsGrabbing():
